e34155a151c4016bd4a611843795cebe1599e10d
DJ <dj.bowden7@gmail.com>
2020-10-26 19:12:14 -0400

    added post route, refactored to use managed state, refactored dao to store collection reference instead of client reference

--- src/expense_service.rs
+++ src/expense_service.rs
@@ -1,47 +1,51 @@
-
 use futures::stream::StreamExt;
-use mongodb::{Client, bson, bson::doc, bson::oid::ObjectId, error::Result};
-use serde::{Serialize, Deserialize};
+use mongodb::{bson, bson::doc, bson::oid::ObjectId, error::Result, Client, Collection};
+use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Expense {
-    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
-    id: Option<ObjectId>,
-    name: String,
-    recurrance: String,
-    amount: u8
- } 
- pub struct ExpenseDAO<'a> {
-    client: Client,
-    db_name: &'a str,
-    collection: &'a str
- }
- impl<'a> ExpenseDAO<'a> {
-      pub async fn new()  -> Result<ExpenseDAO<'a>> {
-        Ok(ExpenseDAO {
-            client : Client::with_uri_str("mongodb://localhost:27017/").await?,
-            db_name : "finance",
-            collection : "expense"
-        })
-     }
-     pub async fn get_all_expenses(&self) -> Result<Vec<Expense>>{
-         let expenses = self.client.database(self.db_name).collection(self.collection);
-         let mut results = expenses.find(None,None).await?;
-         let mut result_vec: Vec<Expense> = Vec::new();
-         while let Some(doc) = results.next().await {
-            let expense: Expense = bson::from_bson(bson::Bson::Document(doc?))?;
-            result_vec.push(expense);
-         }
-         Ok(result_vec)
-    }
-    pub async fn get_expense_by_id(&self,id: String) -> Result<Expense>{
-         let expenses = self.client.database(self.db_name).collection(self.collection);
-         println!("id is {}",id);
-         let doc = expenses.find_one(doc! {
-            "_id" : ObjectId::with_string(id.as_str()).expect("something went wrong")
-         },
-         None).await?;
-         let expense : Expense  = bson::from_bson(bson::Bson::Document(doc.unwrap()))?;
-         Ok(expense)
+   #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+   id: Option<ObjectId>,
+   name: String,
+   recurrance: String,
+   amount: u8,
+}
+pub struct ExpenseDAO {
+   collection: Collection,
+}
+impl ExpenseDAO {
+   pub async fn new() -> Result<ExpenseDAO> {
+      let client = Client::with_uri_str("mongodb://localhost:27017/").await?;
+      let collection = client.database("finance").collection("expense");
+      Ok(ExpenseDAO {
+         collection: collection,
+      })
    }
- }
-
+   pub async fn get_all_expenses(&self) -> Result<Vec<Expense>> {
+      let mut results = self.collection.find(None, None).await?;
+      let mut result_vec: Vec<Expense> = Vec::new();
+      while let Some(doc) = results.next().await {
+         let expense: Expense = bson::from_bson(bson::Bson::Document(doc?))?;
+         result_vec.push(expense);
+      }
+      Ok(result_vec)
+   }
+   pub async fn get_expense_by_id(&self, id: String) -> Result<Expense> {
+      let doc = self
+         .collection
+         .find_one(
+            doc! {
+               "_id" : ObjectId::with_string(id.as_str()).expect("something went wrong")
+            },
+            None,
+         )
+         .await?;
+      let expense: Expense = bson::from_bson(bson::Bson::Document(doc.unwrap()))?;
+      Ok(expense)
+   }
+   pub async fn create_expense(&self, expense: Expense) -> Result<String> {
+      let expense_bson = bson::to_bson(&expense)?;
+      let document = expense_bson.as_document().unwrap();
+      let result = self.collection.insert_one(document.to_owned(), None).await?;
+      Ok(result.inserted_id.as_str().unwrap().to_owned())
+   }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,35 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-#[macro_use] extern crate rocket;
-use rocket_contrib::json::Json;
+#[macro_use]
+extern crate rocket;
 use rocket::response::status::NotFound;
+use rocket::State;
+use rocket_contrib::json::Json;
 mod expense_service;
 
 #[get("/")]
-async fn index() ->  String {
-    
+async fn index() -> String {
     "Hello, world!".to_string()
 }
 
 #[get("/expenses")]
-async fn expenses_all() -> Result<Json<Vec<expense_service::Expense>>, NotFound<String>> {    
-    let expense_dao : expense_service::ExpenseDAO = expense_service::ExpenseDAO::new().await.expect("shit");
-     match expense_dao.get_all_expenses().await {
+async fn expenses_all(expense_dao: State<'_,expense_service::ExpenseDAO>) -> Result<Json<Vec<expense_service::Expense>>, NotFound<String>> {
+    match expense_dao.get_all_expenses().await {
         Ok(expenses) => Ok(Json(expenses)),
-        Err(e) => Err(NotFound(e.to_string()))
+        Err(e) => Err(NotFound(e.to_string())),
     }
 }
 #[get("/expenses/<id>")]
-async fn expense_by_id(id: String) -> Result<Json<expense_service::Expense>, NotFound<String>> {    
-    let expense_dao : expense_service::ExpenseDAO = expense_service::ExpenseDAO::new().await.expect("shit");
-     match expense_dao.get_expense_by_id(id).await {
+async fn expense_by_id(expense_dao: State<'_,expense_service::ExpenseDAO>, id: String) -> Result<Json<expense_service::Expense>, NotFound<String>> {
+    match expense_dao.get_expense_by_id(id).await {
         Ok(expenses) => Ok(Json(expenses)),
-        Err(e) => Err(NotFound(e.to_string()))
+        Err(e) => Err(NotFound(e.to_string())),
+    }
+}
+#[post("/expenses", data = "<expense>")]
+async fn create_expense(expense_dao: State<'_,expense_service::ExpenseDAO>, expense: Json<expense_service::Expense>) -> Result<Json<String>, NotFound<String>> {
+    match expense_dao.create_expense(expense.0).await {
+        Ok(result) => Ok(Json(result)),
+        Err(e) => Err(NotFound(e.to_string())),
     }
 }
 // allows asynchronous main method
@@ -33,6 +39,7 @@ async fn expense_by_id(id: String) -> Result<Json<expense_service::Expense>, Not
 //     rocket::ignite().mount("/", routes![index,test]).launch().await;
 // }
 #[launch]
-fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount("/", routes![index, expenses_all, expense_by_id])
+async fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![index, expenses_all,create_expense, expense_by_id])
+    .manage(expense_service::ExpenseDAO::new().await.expect("shit"))
 }
